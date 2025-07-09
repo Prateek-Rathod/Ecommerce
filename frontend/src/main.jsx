5ba3a34d71bd9f19b67ba63f655d51ca69153960
Prateek-Rathod <prateek12082004@gmail.com>
2025-07-09 08:02:02 +0530

    Your commit message

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { HashRouter } from 'react-router-dom'; // 👈 Change this
+import { HashRouter } from 'react-router-dom'; //  Change this
 import ShopContextProvider from './context/ShopContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter> {/* 👈 Change BrowserRouter to HashRouter */}
+    <HashRouter> {/*  Change BrowserRouter to HashRouter */}
       <ShopContextProvider>
         <App />
       </ShopContextProvider>
